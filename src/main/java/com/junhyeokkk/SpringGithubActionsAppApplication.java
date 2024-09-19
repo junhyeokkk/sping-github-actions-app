3b2c786ecc671cc1003c68fb0589375e95ba990e
Junhyeok <loveu9911@naver.com>
2024-09-19 16:30:16 +0900

    last

--- src/main/java/com/junhyeokkk/SpringGithubActionsAppApplication.java
+++ src/main/java/com/junhyeokkk/SpringGithubActionsAppApplication.java
@@ -3,7 +3,6 @@ package com.junhyeokkk;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
